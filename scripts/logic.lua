c93a2641dc8c30e7fcdbc76bcc6227cc218abe7f
bluejello42 <75703767+bluejello42@users.noreply.github.com>
2024-06-10 12:06:39 -0600

    Add files via upload
    fixed logic error with progressive items

--- scripts/logic.lua
+++ scripts/logic.lua
@@ -24,9 +24,9 @@ function Zone2Access()
         Tracker:ProviderCountForCode("Wetland Aspect") +
         Tracker:ProviderCountForCode("Aphelian Sanctuary")
 		
-	if (value > 0) and (Tracker:ProviderCountForCode("Stage 2") > 0) or (Tracker:ProviderCountForCode("Progressive Stage") > 1) then
+	if (value > 0) and (Tracker:ProviderCountForCode("Stage 2") > 0) then
 		return 1
-	elseif (value > 0 ) and (Tracker:ProviderCountForCode("Progressive Stage") > 1) then
+	elseif (value > 0) and (Tracker:ProviderCountForCode("Progressive Stage") > 1) then
     end
 	
 	return 0
@@ -38,9 +38,9 @@ function Zone3Access()
         Tracker:ProviderCountForCode("Sulfur Pools") +
         Tracker:ProviderCountForCode("Scorched Acres")
 
-	if (value > 0) and (Tracker:ProviderCountForCode("Stage 3") > 0) or (Tracker:ProviderCountForCode("Progressive Stage") > 2) then
+	if (value > 0) and (Tracker:ProviderCountForCode("Stage 3") > 0) then
 		return 1
-	elseif (value > 0 ) and (Tracker:ProviderCountForCode("Progressive Stage") > 2) then
+	elseif (value > 0) and (Tracker:ProviderCountForCode("Progressive Stage") > 2) then
 		return 1
     end
 	
@@ -53,9 +53,9 @@ function Zone4Access()
         Tracker:ProviderCountForCode("Sirens Call") +
         Tracker:ProviderCountForCode("Sundered Grove")
 
-	if (value > 0 ) and (Tracker:ProviderCountForCode("Stage 4") > 0) then
+	if (value > 0) and (Tracker:ProviderCountForCode("Stage 4") > 0) then
 		return 1
-	elseif (value > 0 ) and (Tracker:ProviderCountForCode("Progressive Stage") > 3) then
+	elseif (value > 0) and (Tracker:ProviderCountForCode("Progressive Stage") > 3) then
 		return 1
     end
 	
